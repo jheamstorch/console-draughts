342acb98de022ada60e018de121100e273e846b7
jheamstorch <jheam.storch@hotmail.com>
2020-03-21 15:06:43 -0300

    Divided the TwoDimensionsArrayPosition.ToBoardPosition() method into two other methods called FromArrayRowToBoardRow() and FromArrayColumnToBoardColumn()

--- Board/TwoDimensionsArrayPosition.cs
+++ Board/TwoDimensionsArrayPosition.cs
@@ -17,19 +17,52 @@
 
         //
         // Summary:
+        //     Converts only the array line to its equivalent board row.
+        //
+        // Parameters:
+        //   arrRow:
+        //     The array row to be converted.
+        //
+        //   numberBoardRows:
+        //     The total number of rows in the board.
+        //
+        // Returns:
+        //     The array row's equivalent board line.
+        public static int FromArrayRowToBoardRow(int arrRow, int numberBoardRows)
+        {
+            return numberBoardRows - arrRow;
+        }
+
+        //
+        // Summary:
+        //     Converts only the array column to its equivalent board column.
+        //
+        // Parameters:
+        //   arrColumn:
+        //     The array column to be converted.
+        //
+        // Returns:
+        //     The array column's equivalent board column.
+        public static char FromArrayColumnToBoardColumn(int arrColumn)
+        {
+            return (char)(arrColumn + (int)'a');
+        }
+
+        //
+        // Summary:
         //     Convert the TwoDimensionsArrayPosition to its equivalent BoardPosition.
         //
         // Parameters:
         //   numberBoardRows:
         //     The total number of rows in the board.
         //
-        // Return:
+        // Returns:
         //     A BoardPosition containing the actual TwoDimensionsArrayPosition converted to a BoardPosition.
         public BoardPosition ToBoardPosition(int numberBoardRows)
         {
             return new BoardPosition(
-                numberBoardRows - Row,
-                (char)(Column + (int)'a')
+                FromArrayRowToBoardRow(Row, numberBoardRows),
+                FromArrayColumnToBoardColumn(Column)
             );
         }
 
